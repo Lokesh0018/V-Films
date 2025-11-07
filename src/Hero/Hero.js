2d27666579047c43ae34d45fb5f7918b07ffe653
Lokesh0018 <munakalalokesh222@gmail.com>
2025-11-07 15:11:35 +0530

    Developed Hero Page

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { ReactComponent as MailIcon } from "../asserts/mail.svg";
+import mandala from "../asserts/Hero Mandala.png";
+import logo from "../asserts/VFilms Logo.png";
+
 
 const Hero = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,13 +27,25 @@ const Hero = () => {
                 </button>
               </li>
             </ul>
-
             <div className="menu" onClick={toggleMenu}>
               <div className={`bar top ${isOpen ? "open" : ""}`}></div>
               <div className={`bar bottom ${isOpen ? "open" : ""}`}></div>
             </div>
           </div>
         </nav>
+      </div>
+      <div className="heroContent">
+        <div className="leftContent">
+          <img src={mandala} className="mandalaImg" alt="" />
+          <img src={logo} className="logoImg" alt=""/>
+        </div>
+        <div className="rightContent">
+          <p className="lable1">Varnan is where stories find their voice and form</p>
+          <p className="lable2">Films . Brands . Art</p>
+          <p className="lable3">Since 2009, V’ve been telling stories - stories of people, their journeys, and the places that shape them.
+            Some begin in polished boardrooms, others in humble village squares. But every story starts the same way - by listening with intention. V believes it takes trust, patience, and an eye for the unseen to capture what truly matters.
+            V doesn’t just tell stories - V honors them.</p>
+        </div>
       </div>
     </div>
   );
