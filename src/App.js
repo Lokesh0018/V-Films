7be0cca15f9cddba131fd30f338becf85954d324
Lokesh0018 <munakalalokesh222@gmail.com>
2025-11-09 17:46:15 +0530

    Updated URL

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename='/V-Films'>
         <Navigation />
         <Routes>
           <Route path="/" element={<Hero />} />
